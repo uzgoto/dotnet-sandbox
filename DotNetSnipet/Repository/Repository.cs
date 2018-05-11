cf522116c2b049eaa1b8404319302b1dca5e18fd
uzgoto <Honeywaltz.Melodiou@gmail.com>
2018-05-11 09:56:35 +0900

    craete Repository base class.

---
+++ DotNetSnipet/Repository/Repository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzgoto.DotNetSnipet.Repository
+{
+    public abstract class Repository<TEntity>
+    {
+        protected IEnumerable<TEntity> EnumerateDataReader(IDbCommand command)
+        {
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    yield return CreateEntity(reader);
+                }
+            }
+        }
+
+        private TEntity CreateEntity(IDataRecord record)
+        {
+            var entityProps = typeof(TEntity).GetProperties();
+            if (record.FieldCount != entityProps.Length)
+            {
+                throw new IndexOutOfRangeException(
+                    $"Field count is not match. {nameof(IDataRecord)} is {record.FieldCount} <-> {nameof(TEntity)} is {entityProps.Length}");
+            }
+
+            var entity = Activator.CreateInstance<TEntity>();
+            foreach (var prop in entityProps)
+            {
+                prop.SetValue(entity, record[prop.Name]);
+            }
+            return entity;
+        }
+    }
+}
