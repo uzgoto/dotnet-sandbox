c83811a07b56492fdfa60db168e84018c48e707c
uzgoto <Honeywaltz.Melodiou@gmail.com>
2018-05-29 09:18:14 +0900

    Add to deal process argument components

---
+++ DotNetSnipet/WinForms/InterProc/Helper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Uzgoto.DotNetSnipet.WinForms.InterProc
+{
+    public sealed class Helper<TArgument> where TArgument : struct
+    {
+        private static PropertyInfo[] Properties =>
+            typeof(TArgument)
+                .GetProperties()
+                .Where(prop => prop.GetCustomAttributes<ProcessArgumentAttribute>().Any())
+                .OrderBy(prop => prop.GetCustomAttribute<ProcessArgumentAttribute>().Ordianl)
+                .ToArray();
+
+        public static TArgument Create(string[] args)
+        {
+            if (args == null)
+            {
+                throw new ArgumentException("Argument is null.", nameof(args));
+            }
+            if (args.Length != Properties.Length)
+            {
+                throw new ArgumentException($"Argument length is not match.", nameof(args));
+            }
+
+            var argument = new TArgument();
+            foreach(var anon in Properties.Zip(args, (prop, value) => new { Prop = prop, Value = value }))
+            {
+                anon.Prop.SetValue(argument, anon.Value);
+            }
+            return argument;
+        }
+
+        public static IEnumerable<string> ConvertToProcessArgs(TArgument argument)
+        {
+            return
+                Properties.Select(prop => Convert.ToString(prop.GetValue(argument)));
+        }
+    }
+}
---
+++ DotNetSnipet/WinForms/InterProc/ProcessArgumentAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uzgoto.DotNetSnipet.WinForms.InterProc
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public sealed class ProcessArgumentAttribute : Attribute
+    {
+        public int Ordianl { get; private set; }
+        public ProcessArgumentAttribute(int ordinal)
+        {
+            if (ordinal <= 0)
+            {
+                throw new ArgumentException($"{ordinal} is more than equals 1.");
+            }
+            this.Ordianl = ordinal;
+        }
+    }
+}
