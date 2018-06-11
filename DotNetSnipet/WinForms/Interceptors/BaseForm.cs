7ef1abfe6d259864bbd71fd7a4d15aad1effce63
uzgoto <Honeywaltz.Melodiou@gmail.com>
2018-06-11 23:32:33 +0900

    rename some variables

--- DotNetSnipet/WinForms/Interceptors/BaseForm.cs
+++ DotNetSnipet/WinForms/Interceptors/BaseForm.cs
@@ -11,19 +11,18 @@ namespace Uzgoto.DotNetSnipet.WinForms.Interceptors
 {
     public partial class BaseForm : Form
     {
-        private static FieldInfo eventClickField =
+        private static FieldInfo EventClickField =>
             typeof(Control).GetField("EventClick", BindingFlags.NonPublic | BindingFlags.Static);
-        private static EventInfo clickEvent =
-            typeof(Control).GetEvent(nameof(Click));
-        private static PropertyInfo controlEvents =
+        private static EventInfo ControlClickEvent => typeof(Control).GetEvent(nameof(Click));
+        private static PropertyInfo ControlEventsProperty =>
             typeof(Control).GetProperty(nameof(Events), BindingFlags.Instance | BindingFlags.NonPublic);
-        private static MethodInfo interceptor =
-            typeof(BaseForm).GetMethod(nameof(Validate), BindingFlags.Instance | BindingFlags.NonPublic);
+        private static MethodInfo InterceptMethod =>
+            typeof(BaseForm).GetMethod(nameof(Sanitaize), BindingFlags.Instance | BindingFlags.NonPublic);
 
-        private Dictionary<Control, List<Delegate>> _handlers = new Dictionary<Control, List<Delegate>>();
+        private Dictionary<Control, List<Delegate>> _tmpHandlers = new Dictionary<Control, List<Delegate>>();
 
-        protected Dictionary<Control, InputType> ValidationTargets { get; set; }
-        protected IEnumerable<Control> ValidationTrrigers { get; set; }
+        protected Dictionary<Control, InputType> SanitizationTargets { get; set; }
+        protected IEnumerable<Control> Interceptors { get; set; }
 
         public BaseForm()
         {
@@ -35,47 +34,55 @@ namespace Uzgoto.DotNetSnipet.WinForms.Interceptors
         {
             base.OnLoad(e);
 
-            var targetControls = this.EnumerateControlsWithAttribute<ValidationTargetAttribute>(this);
-            this.ValidationTargets = targetControls.ToDictionary(ctrl => ctrl.Item1, ctrl => ctrl.Item2.InputType);
+            this.SanitizationTargets =
+                this.EnumerateControls<ValidationTargetAttribute>(this)
+                    .ToDictionary(
+                        target => target.control,
+                        target => target.attribute.InputType);
+            if (this.SanitizationTargets == null) return;
 
-            var trrigerControls = this.EnumerateControlsWithAttribute<InterceptAttribute>(this);
-            this.ValidationTrrigers = trrigerControls.Select(ctrl => ctrl.Item1);
+            this.Interceptors =
+                this.EnumerateControls<InterceptAttribute>(this)
+                    .Select(ctrl => ctrl.control);
+            if (this.Interceptors == null) return;
 
-            if (this.ValidationTargets == null || this.ValidationTargets == null) return;
-
-            foreach (var control in this.ValidationTrrigers)
+            foreach (var control in this.Interceptors)
             {
-                var handlers = controlEvents.GetValue(control, null) as EventHandlerList;
-                var eventKey = eventClickField.GetValue(control);
-                var clickEventHandler = handlers[eventKey];
+                // Get EventHandlerList from Control.Events property.
+                var eventHandlers = ControlEventsProperty.GetValue(control, null) as EventHandlerList;
+                var eventKey = EventClickField.GetValue(control);
+                // Get Click EventHandler.
+                var clickEventHandler = eventHandlers[eventKey];
                 if (clickEventHandler != null)
                 {
                     var clickDelegates = clickEventHandler.GetInvocationList();
-                    if (!this._handlers.ContainsKey(control))
+                    if (!this._tmpHandlers.ContainsKey(control))
                     {
-                        this._handlers.Add(control, new List<Delegate>());
+                        this._tmpHandlers.Add(control, new List<Delegate>());
                     }
-                    this._handlers[control].AddRange(clickDelegates);
+                    this._tmpHandlers[control].AddRange(clickDelegates);
 
-                    Array.ForEach(clickDelegates, dlgt => handlers.RemoveHandler(eventKey, dlgt));
+                    Array.ForEach(clickDelegates, dlgt => eventHandlers.RemoveHandler(eventKey, dlgt));
 
-                    clickEvent.GetAddMethod().Invoke(control, new object[] { Delegate.CreateDelegate(clickEvent.EventHandlerType, this, interceptor) });
+                    ControlClickEvent.GetAddMethod().Invoke(
+                        control,
+                        new object[] { Delegate.CreateDelegate(ControlClickEvent.EventHandlerType, this, InterceptMethod) });
                 }
             }
         }
 
-        private void Validate(object sender, EventArgs e)
+        private void Sanitaize(object sender, EventArgs e)
         {
-            var msg = this.ValidationTargets.Select(target => $"{target.Key.GetType().Name}, {target.Key.Text}, {target.Value.ToString()}");
+            var msg = this.SanitizationTargets.Select(target => $"{target.Key.GetType().Name}, {target.Key.Text}, {target.Value.ToString()}");
             MessageBox.Show(string.Join(Environment.NewLine, msg));
 
-            foreach (var dlgt in this._handlers[sender as Control])
+            foreach (var dlgt in this._tmpHandlers[sender as Control])
             {
                 dlgt.DynamicInvoke(sender, e);
             }
         }
 
-        private IEnumerable<Tuple<Control, T>> EnumerateControlsWithAttribute<T>(Form form) where T : Attribute
+        private IEnumerable<(Control control, TAttribute attribute)> EnumerateControls<TAttribute>(Form form) where TAttribute : Attribute
         {
             if (form == null) throw new ArgumentNullException(nameof(form));
             if (form.Controls == null) throw new ArgumentException($"{nameof(form)} has no controls.");
@@ -86,11 +93,12 @@ namespace Uzgoto.DotNetSnipet.WinForms.Interceptors
             foreach (var field in fields)
             {
                 var value = field.GetValue(form);
-                if (value is Control)
+                if (value is Control control)
                 {
-                    if (field.GetCustomAttribute<T>() != null)
+                    var attribute = field.GetCustomAttribute<TAttribute>();
+                    if (attribute != null)
                     {
-                        yield return Tuple.Create(value as Control, field.GetCustomAttribute<T>());
+                        yield return (control, attribute);
                     }
                 }
             }
