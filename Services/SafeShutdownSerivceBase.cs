0f4d7881cd2cb4ba2da536f5752b189bb1fe87bc
uzgoto <Honeywaltz.Melodiou@gmail.com>
2018-08-12 10:21:09 +0900

    create services project.

---
+++ Services/SafeShutdownSerivceBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace Uzgoto.DotNetSnipet.Services
+{
+    abstract class SafeShutdownSerivceBase : ServiceBase
+    {
+        protected bool AllowShutdown { get; set; }
+        protected new EventLog EventLog { get; set; }
+
+        protected SafeShutdownSerivceBase(string serviceName) : base()
+        {
+            this.AutoLog = false;
+            this.CanShutdown = true;
+            this.CanStop = false;
+            this.EventLog = new EventLog(logName: "CustomApp", machineName: "localhost", source: serviceName);
+            this.ServiceName = serviceName;
+        }
+
+        protected abstract bool IsArgumentsValid(string[] args);
+
+        protected override void OnShutdown()
+        {
+            while (!this.AllowShutdown)
+            {
+                this.EventLog.WriteEntry($"{this.ServiceName}: シャットダウンを待機します。", EventLogEntryType.Information, 0);
+            }
+            this.EventLog.WriteEntry($"{this.ServiceName}: シャットダウンします。", EventLogEntryType.Information, 0);
+            base.OnShutdown();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if(this.IsArgumentsValid(args))
+            {
+                this.EventLog.WriteEntry($"{this.ServiceName}: 起動パラメータが不正です。\n{string.Join("\n", args)}", EventLogEntryType.Error, 100);
+                return;
+            }
+            base.OnStart(args);
+            this.EventLog.WriteEntry($"{this.ServiceName}: 起動しました。\n{string.Join("\n", args)}", EventLogEntryType.Information, 0);
+        }
+    }
+}
---
+++ Services/Service1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uzgoto.DotNetSnipet.Services;
+
+namespace Uzgoto.DotNetSnipet.Sample
+{
+    class Service1 : SafeShutdownSerivceBase
+    {
+        private int DelaySeconds;
+        public static void Main(string[] args)
+        {
+
+        }
+
+        private Service1(string serviceName) : base(serviceName)
+        {
+
+        }
+
+        protected override bool IsArgumentsValid(string[] args)
+        {
+            if(args.Length != 1) { return false; }
+            return int.TryParse(args[0], out this.DelaySeconds);
+        }
+    }
+}
