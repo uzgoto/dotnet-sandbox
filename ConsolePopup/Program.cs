88073582e9e13ba58be8a886ffc8253efa1ba28b
uzgoto <Honeywaltz.Melodiou@gmail.com>
2018-08-14 00:47:04 +0900

    create project popup message in consoleapp.

---
+++ ConsolePopup/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Uzgoto.Dotnet.Sandbox.Winapi;
+
+namespace Uzgoto.Dotnet.Sandbox.ConsolePopup
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var flag = true;
+            var sw = new Stopwatch();
+            while (true)
+            {
+                sw.Start();
+                if (flag)
+                {
+
+                    Console.WriteLine($"Elapsed (1-1-1): {sw.ElapsedMilliseconds}");
+                    var dialogs = Dialog.EnumerateChilds();
+                    foreach(var dialog in dialogs)
+                    {
+                        dialog.Close();
+                    }
+                    Console.WriteLine($"Elapsed (1-1-2): {sw.ElapsedMilliseconds}");
+                    
+                    Task.Factory.StartNew(() =>
+                    {
+                        Console.WriteLine($"Elapsed (1-2-1): {sw.ElapsedMilliseconds}");
+                        MessageBox.Show(
+                            $"{sw.ElapsedMilliseconds}",
+                            $"{Process.GetCurrentProcess().ProcessName}",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Warning,
+                            MessageBoxDefaultButton.Button1,
+                            MessageBoxOptions.ServiceNotification);
+                        Console.WriteLine($"Elapsed (1-2-2): {sw.ElapsedMilliseconds}");
+                    }).ConfigureAwait(true);
+
+                    Console.WriteLine($"Elapsed (1-3-1): {sw.ElapsedMilliseconds}");
+                    Task.Delay(5000).Wait();
+                    flag = false;
+                    Console.WriteLine($"Elapsed (1-3-2): {sw.ElapsedMilliseconds}");
+                }
+                else
+                {
+                    Console.WriteLine($"Elapsed (2-1-1): {sw.ElapsedMilliseconds}");
+                    var dialogs = Dialog.EnumerateChilds();
+                    foreach (var dialog in dialogs)
+                    {
+                        dialog.Close();
+                    }
+                    Console.WriteLine($"Elapsed (2-1-2): {sw.ElapsedMilliseconds}");
+                    
+                    Task.Factory.StartNew(() =>
+                    {
+                        Console.WriteLine($"Elapsed (2-2-1): {sw.ElapsedMilliseconds}");
+                        MessageBox.Show(
+                            $"{sw.ElapsedMilliseconds}",
+                            $"{Process.GetCurrentProcess().ProcessName}",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information,
+                            MessageBoxDefaultButton.Button1,
+                            MessageBoxOptions.ServiceNotification);
+                        Console.WriteLine($"Elapsed (2-2-2): {sw.ElapsedMilliseconds}");
+                    }).ConfigureAwait(true);
+
+                    Console.WriteLine($"Elapsed (2-3-1): {sw.ElapsedMilliseconds}");
+                    Task.Delay(5000).Wait();
+                    flag = true;
+                    Console.WriteLine($"Elapsed (2-3-2): {sw.ElapsedMilliseconds}");
+                }
+            }
+        }
+    }
+}
