944037c57439f75b0374c203cf28c1a071e15d8a
uzgoto <Honeywaltz.Melodiou@gmail.com>
2018-08-13 15:37:53 +0900

    create project winapiwrapper.

---
+++ WinApiWrapper/Extensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Uzgoto.DotNet.Sandbox.WinApiWrapper
+{
+    internal static class Extensions
+    {
+        private static class WinApi
+        {
+            [DllImport("user32.dll", SetLastError = true)]
+            internal static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+            [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+            internal static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+            [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+            internal static extern int GetWindowText(IntPtr hWnd, StringBuilder lpText, int nMaxCount);
+        }
+
+        internal static IEnumerable<IntPtr> EnumWindowHandles(this IntPtr hWndRoot)
+        {
+            yield return hWndRoot;
+
+            var child = IntPtr.Zero;
+            while ((child = WinApi.FindWindowEx(hWndRoot, child, null, null)) != IntPtr.Zero)
+            {
+                foreach (var childchild in EnumWindowHandles(child))
+                {
+                    yield return childchild;
+                }
+            }
+        }
+
+        internal static string GetClassName(this IntPtr hWnd)
+        {
+            var builder = new StringBuilder(256);
+            var ret = WinApi.GetClassName(hWnd, builder, builder.Capacity);
+            return builder.ToString();
+        }
+
+        internal static string GetWindowText(this IntPtr hWnd)
+        {
+            var builder = new StringBuilder(256);
+            var ret = WinApi.GetWindowText(hWnd, builder, builder.Capacity);
+            return builder.ToString();
+        }
+    }
+}
---
+++ WinApiWrapper/Window.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Uzgoto.DotNet.Sandbox.WinApiWrapper
+{
+    public class Window
+    {
+        public IntPtr Handle { get; private set; }
+        public string Title { get; private set; }
+        public string ClassName { get; private set; }
+
+        public static IEnumerable<Window> EnumerateChildWindows(Process parent)
+        {
+            foreach (var handle in parent.MainWindowHandle.EnumWindowHandles())
+            {
+                yield return new Window()
+                {
+                    Handle = handle,
+                    Title = handle.GetWindowText(),
+                    ClassName = handle.GetClassName(),
+                };
+            }
+        }
+
+        public override string ToString()
+        {
+            return
+                string.Format("{{{0}:{1}, {2}:{3}, {4}:{5}}}",
+                    nameof(this.Handle), this.Handle,
+                    nameof(this.Title), this.Title,
+                    nameof(this.ClassName), this.ClassName);
+        }
+
+    }
+}
