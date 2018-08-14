6d381535df748fcd85a026f7e0cfd876d9cccad8
uzgoto <Honeywaltz.Melodiou@gmail.com>
2018-08-14 11:15:52 +0900

    create sample programs.

--- ConsolePopup/Program.cs
+++ ConsolePopup/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Uzgoto.Dotnet.Sandbox.Winapi;
@@ -8,70 +9,74 @@ namespace Uzgoto.Dotnet.Sandbox.ConsolePopup
 {
     public class Program
     {
+        private static volatile bool canConnected = true;
+        private static readonly object Lock = new object();
+        private static readonly string LogFormat = "[{0,-10}][{1,-5}][{2,-10}] Elapsed:{3:hh\\:mm\\:ss\\.ffffff} {4}";
+        private const int MainProcessDelaySeconds = 5;
+
         static void Main(string[] args)
         {
-            var flag = true;
             var sw = new Stopwatch();
+            sw.Start();
+
+            // Switch 'canConnected' random value asynchronously.
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    var randomValue = new Random(sw.Elapsed.Milliseconds).Next();
+                    var delaySeconds = randomValue % (60 - MainProcessDelaySeconds) + MainProcessDelaySeconds;
+
+                    Console.WriteLine(LogFormat, "SwitchProc", "begin", "delay", sw.Elapsed, delaySeconds);
+                    Task.Delay(TimeSpan.FromSeconds(delaySeconds)).Wait();
+                    Console.WriteLine(LogFormat, "SwitchProc", "end", "delay", sw.Elapsed, delaySeconds);
+
+                    lock (Lock)
+                    {
+                        Console.WriteLine(LogFormat, "SwitchProc", "begin", "switch", sw.Elapsed, canConnected);
+                        canConnected = delaySeconds % 2 == 0;
+                        Console.WriteLine(LogFormat, "SwitchProc", "end", "switch", sw.Elapsed, canConnected);
+                    }
+                }
+            });
+
+            bool previousConnected = true;
             while (true)
             {
-                sw.Start();
-                if (flag)
+                bool currentConnected;
+                lock(Lock)
                 {
-
-                    Console.WriteLine($"Elapsed (1-1-1): {sw.ElapsedMilliseconds}");
-                    var dialogs = Dialog.EnumerateChilds();
-                    foreach(var dialog in dialogs)
-                    {
-                        dialog.Close();
-                    }
-                    Console.WriteLine($"Elapsed (1-1-2): {sw.ElapsedMilliseconds}");
-                    
-                    Task.Factory.StartNew(() =>
-                    {
-                        Console.WriteLine($"Elapsed (1-2-1): {sw.ElapsedMilliseconds}");
-                        MessageBox.Show(
-                            $"{sw.ElapsedMilliseconds}",
-                            $"{Process.GetCurrentProcess().ProcessName}",
-                            MessageBoxButtons.OK,
-                            MessageBoxIcon.Warning,
-                            MessageBoxDefaultButton.Button1,
-                            MessageBoxOptions.ServiceNotification);
-                        Console.WriteLine($"Elapsed (1-2-2): {sw.ElapsedMilliseconds}");
-                    }).ConfigureAwait(true);
-
-                    Console.WriteLine($"Elapsed (1-3-1): {sw.ElapsedMilliseconds}");
-                    Task.Delay(5000).Wait();
-                    flag = false;
-                    Console.WriteLine($"Elapsed (1-3-2): {sw.ElapsedMilliseconds}");
+                    currentConnected = canConnected;
                 }
-                else
+
+                if (currentConnected != previousConnected)
                 {
-                    Console.WriteLine($"Elapsed (2-1-1): {sw.ElapsedMilliseconds}");
-                    var dialogs = Dialog.EnumerateChilds();
-                    foreach (var dialog in dialogs)
+                    if (currentConnected)
                     {
-                        dialog.Close();
+                        // Close if any messageboxes showed, and show messagebox asynchronously, by service account.
+                        Console.WriteLine(LogFormat, "MainProc", "begin", "open", sw.Elapsed, "Information");
+                        SystemNotifyDialog.ShowInformationAsync(
+                            $"Connection reopened at {DateTime.UtcNow.ToString("yyyy/MM/dd HH:mm:ss.ffffff")}",
+                            $"{Process.GetCurrentProcess().ProcessName}");
+                        Console.WriteLine(LogFormat, "MainProc", "end", "open", sw.Elapsed, "Information");
                     }
-                    Console.WriteLine($"Elapsed (2-1-2): {sw.ElapsedMilliseconds}");
-                    
-                    Task.Factory.StartNew(() =>
+                    else
                     {
-                        Console.WriteLine($"Elapsed (2-2-1): {sw.ElapsedMilliseconds}");
-                        MessageBox.Show(
-                            $"{sw.ElapsedMilliseconds}",
-                            $"{Process.GetCurrentProcess().ProcessName}",
-                            MessageBoxButtons.OK,
-                            MessageBoxIcon.Information,
-                            MessageBoxDefaultButton.Button1,
-                            MessageBoxOptions.ServiceNotification);
-                        Console.WriteLine($"Elapsed (2-2-2): {sw.ElapsedMilliseconds}");
-                    }).ConfigureAwait(true);
-
-                    Console.WriteLine($"Elapsed (2-3-1): {sw.ElapsedMilliseconds}");
-                    Task.Delay(5000).Wait();
-                    flag = true;
-                    Console.WriteLine($"Elapsed (2-3-2): {sw.ElapsedMilliseconds}");
+                        // Close if any messageboxes showed, and show messagebox asynchronously, by service account.
+                        Console.WriteLine(LogFormat, "MainProc", "begin", "open", sw.Elapsed, "Warning");
+                        SystemNotifyDialog.ShowWarningAsync(
+                            $"Connection broken at {DateTime.UtcNow.ToString("yyyy/MM/dd HH:mm:ss.ffffff")}",
+                            $"{Process.GetCurrentProcess().ProcessName}");
+                        Console.WriteLine(LogFormat, "MainProc", "end", "open", sw.Elapsed, "Warning");
+                    }
                 }
+
+                // Delay.
+                //Console.WriteLine(LogFormat, "MainProc", "begin", "delay", sw.Elapsed, MainProcessDelaySeconds);
+                Task.Delay(TimeSpan.FromSeconds(MainProcessDelaySeconds)).Wait();
+                //Console.WriteLine(LogFormat, "MainProc", "end", "delay", sw.Elapsed, MainProcessDelaySeconds);
+
+                previousConnected = currentConnected;
             }
         }
     }
---
+++ ConsolePopup/SystemNotifyDialog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Uzgoto.Dotnet.Sandbox.Winapi;
+
+namespace Uzgoto.Dotnet.Sandbox.ConsolePopup
+{
+    public static class SystemNotifyDialog
+    {
+        private static readonly MessageBoxButtons Buttons = MessageBoxButtons.OK;
+        private static readonly MessageBoxDefaultButton DefaultButton = MessageBoxDefaultButton.Button1;
+        private static readonly MessageBoxOptions Options = MessageBoxOptions.ServiceNotification;
+
+        public static Task<DialogResult> ShowInformationAsync(string text, string caption)
+        {
+            Array.ForEach(Dialog.EnumerateChilds().ToArray(), d => d.Close());
+            return
+                Task.Factory.StartNew(() =>
+                    MessageBox.Show(text, caption, Buttons, MessageBoxIcon.Information, DefaultButton, Options)
+                );
+        }
+
+        public static Task<DialogResult> ShowWarningAsync(string text, string caption)
+        {
+            Array.ForEach(Dialog.EnumerateChilds().ToArray(), d => d.Close());
+            return
+                Task.Factory.StartNew(() =>
+                    MessageBox.Show(text, caption, Buttons, MessageBoxIcon.Warning, DefaultButton, Options)
+                );
+        }
+    }
+}
